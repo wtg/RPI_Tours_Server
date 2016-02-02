93ebb6982851a710ff17c856059b1368bed24168
Sidney Kochman <sidney@kochman.org>
2016-02-02 16:40:41 -0500

    Add /tours endpoint with dummy data

--- server.py
+++ server.py
@@ -2,9 +2,32 @@ import flask
 
 app = flask.Flask(__name__)
 
+def make_tour():
+	tour = {
+		'id': 1,
+		'name': 'Test Tour',
+		'route': [
+			{
+				'description': 'This is a description of this place.',
+				'photos': ['photo1.jpg', 'photo2.jpg'],
+				'coordinate': (3, 4),
+			}, {
+				'coordinate': (2, 3),
+			}, {
+				'coordinate': (4, 1)
+			}
+		]
+	}
+	return tour
+
 @app.route('/')
 def index():
 	return flask.jsonify(hello='world')
+
+@app.route('/tours')
+def tours():
+	tour_lst = [make_tour()]
+	return flask.jsonify(tours=tour_lst)
 
 if __name__ == '__main__':
 	app.run(debug=True)
