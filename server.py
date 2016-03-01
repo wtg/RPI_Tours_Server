f1c91b2c2fe5043626043307f8adc27e9f2e2c27
Sidney Kochman <sidney@kochman.org>
2016-03-01 15:36:15 -0500

    Add CRUD operations for tours

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import flask
 import psycopg2
+import psycopg2.extras
 
 DB_URL = 'postgres://rpitours:rpitours@localhost:5432/rpitours'
 
@@ -11,10 +12,20 @@ def get_db():
 		db = flask.g.database = psycopg2.connect(DB_URL)
 	return db
 
+def init_db():
+	db = psycopg2.connect(DB_URL)
+	cur = db.cursor()
+	cur.execute("""
+		CREATE TABLE IF NOT EXISTS tours (
+			id serial PRIMARY KEY,
+			tour json NOT NULL
+		);
+	""")
+	db.commit()
+
 def make_tour():
 	tour = {
-		'id': 1,
-		'name': 'Test Tour',
+		'name': 'Example Tour',
 		'waypoints': [
 			(5, 2),
 			(2, 3),
@@ -43,7 +54,64 @@ def index():
 @app.route('/tours')
 def tours():
 	tour_lst = [make_tour()]
+
+	db = get_db()
+	cur = db.cursor()
+	cur.execute("SELECT id, tour FROM tours")
+	for row in cur.fetchall():
+		tour = row[1]
+		tour_id = row[0]
+		tour['id'] = tour_id
+		tour_lst.append(tour)
+
 	return flask.jsonify(tours=tour_lst)
+
+@app.route('/tours/create', methods=('POST',))
+def tour_create():
+	tour = make_tour()
+
+	db = get_db()
+	cur = db.cursor()
+	cur.execute("INSERT INTO tours (tour) VALUES (%s) RETURNING id;", (psycopg2.extras.Json(tour),))
+	row = cur.fetchone()
+	tour_id = row[0]
+
+	tour['id'] = tour_id
+
+	# add tour ID to JSON in database
+	cur.execute("UDPATE tours SET tour = %s WHERE id = %s;", (psycopg2.extras.Json(tour), tour_id))
+	db.commit()
+
+	return flask.jsonify(tour)
+
+@app.route('/tours/<int:tour_id>', methods=('GET', 'PUT', 'DELETE'))
+def tour(tour_id):
+	db = get_db()
+	cur = db.cursor()
+
+	if flask.request.method == 'GET':
+		cur.execute("SELECT tour FROM tours WHERE id = %s", (tour_id,))
+		row = cur.fetchone()
+		tour = row[0]
+
+		return flask.jsonify(tour)
+
+	if flask.request.method == 'PUT':
+		tour = flask.request.get_json()
+		if tour is not None:
+			tour['id'] = tour_id
+			cur.execute("UPDATE tours SET tour = %s WHERE id = %s", (psycopg2.extras.Json(tour), tour_id))
+			db.commit()
+
+			return flask.jsonify(tour)
+		else:
+			flask.abort(400)
+
+	if flask.request.method == 'DELETE':
+		cur.execute("DELETE FROM tours WHERE id = %s", (tour_id,))
+		db.commit()
+
+		return flask.jsonify(success=True)
 
 @app.route('/dbtest')
 def dbtest():
@@ -51,4 +119,5 @@ def dbtest():
 	return flask.jsonify(success=True)
 
 if __name__ == '__main__':
+	init_db()
 	app.run(debug=True)
